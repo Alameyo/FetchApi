c2b3e9252b17298d4608d0b92242a4b96e474e79
Alameyo <Alameyo@users.noreply.github.com>
2021-10-30 18:43:35 +0200

    strict tls 1.3

--- src/main/kotlin/com/fetchapi/http/GenericHttpClient.kt
+++ src/main/kotlin/com/fetchapi/http/GenericHttpClient.kt
@@ -1,14 +1,24 @@
 package com.fetchapi.http
 
+import java.lang.System.setProperty
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.newBuilder
 import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers.ofString
+import javax.net.ssl.SSLParameters
 
 class GenericHttpClient {
-    private val httpClient = HttpClient.newHttpClient()
+
+    init {
+        setProperty("jdk.tls.namedGroups", "secp521r1")
+    }
+    private val strictSSLParameters = SSLParameters().apply {
+        protocols = arrayOf("TLSv1.3")
+    }
+
+    private val httpClient = HttpClient.newBuilder().sslParameters(strictSSLParameters).build()
 
     fun httpGet(addressUri: URI): HttpResponse<String>? {
         val httpRequest = newBuilder().uri(addressUri).build()
