5f4f53b59abc78980e112eebefa752d42fedf363
Alameyo <Alameyo@users.noreply.github.com>
2021-10-30 19:15:14 +0200

    4 no magic number

--- src/main/kotlin/com/fetchapi/validation/PostValidator.kt
+++ src/main/kotlin/com/fetchapi/validation/PostValidator.kt
@@ -3,6 +3,8 @@ package com.fetchapi.validation
 import com.google.gson.JsonObject
 
 class PostValidator {
+    private val numberOfFieldsInPost = 4
+
     fun validate(post: JsonObject) {
         val userId = post.get("userId")?.asLong
         val id = post.get("id")?.asLong
@@ -10,7 +12,7 @@ class PostValidator {
         val body = post.get("body")?.asString
 
         when {
-            post.size() != 4 -> throw InputValidationException("Incorrect number of elements in post")
+            post.size() != numberOfFieldsInPost -> throw InputValidationException("Incorrect number of elements in post")
             userId == null || userId < 0 -> throw InputValidationException("useId invalid")
             id == null || id < 0 -> throw InputValidationException("id invalid")
             title == null -> throw InputValidationException("Post doesn't have title field")
