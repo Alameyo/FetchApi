ddca1dfc79e2badf741a09a4f6af290c96e03a60
Alameyo <Alameyo@users.noreply.github.com>
2021-10-30 14:31:39 +0200

    add detekt

--- src/main/kotlin/PostSaver.kt
+++ src/main/kotlin/PostSaver.kt
@@ -1,17 +1,10 @@
 import com.google.gson.JsonObject
-import java.lang.Exception
 
 class PostSaver {
     private val fileSaver = FileSaver()
     private fun createPath(pathPrefix: String, jsonElement: JsonObject): String {
-        val path: String
-
-        val id = jsonElement.get("id").asString
-        when {
-            id.isNotBlank() -> path = "$pathPrefix$id.json"
-            else -> throw Exception("No id in the post")
-        }
-        return path
+        val id = jsonElement.get("id").asLong
+        return "$pathPrefix$id.json"
     }
 
     fun save(jsonObject: JsonObject, pathPrefix: String = "") {
--- src/test/kotlin/PostSaverTest.kt
+++ src/test/kotlin/PostSaverTest.kt
@@ -6,8 +6,10 @@ import java.io.File
 
 class PostSaverTest {
     private val postSaver = PostSaver()
+    @Suppress("MaxLineLength")
     private val examplePostString =
-        """{"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"}""".trim()
+        """{"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"}"""
+            .trim()
     private val examplePost = parseString(examplePostString) as JsonObject
     private val expectedFilePath = "2.json"
 
