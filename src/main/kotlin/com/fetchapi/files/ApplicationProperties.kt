fed884ff013e63ca089093bf2a8232bfa4d90b2b
Alameyo <Alameyo@users.noreply.github.com>
2021-10-30 22:24:20 +0200

    stylo

--- src/main/kotlin/com/fetchapi/files/ApplicationProperties.kt
+++ src/main/kotlin/com/fetchapi/files/ApplicationProperties.kt
@@ -2,7 +2,6 @@ package com.fetchapi.files
 
 import java.util.Properties
 
-
 class ApplicationProperties {
     private val inputStream = this::class.java.getResourceAsStream("/app.properties")
     private val properties = Properties().apply {
