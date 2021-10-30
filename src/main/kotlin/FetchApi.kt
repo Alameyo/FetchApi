91bcbd7df1d17c1276fe4abb151b1aa2cbee4a9d
Alameyo <Alameyo@users.noreply.github.com>
2021-10-30 16:42:40 +0200

    imports

--- src/main/kotlin/FetchApi.kt
+++ src/main/kotlin/FetchApi.kt
@@ -1,11 +1,12 @@
 import com.google.gson.JsonObject
+import java.io.File.separator
 import java.lang.System.getProperty
 
 private val fetchApiClient = FetchPostsClient()
 private val postsDivider = PostsDivider()
 private val postValidator = PostValidator()
 private val postSaver = PostSaver()
-private var directoryToSaveFiles = "${getProperty("user.dir")}${java.io.File.separator}results${java.io.File.separator}"
+private var directoryToSaveFiles = "${getProperty("user.dir")}${separator}results$separator"
 
 fun main(args: Array<String>) {
     if(args.isNotEmpty() && args[0].isNotBlank()) {
